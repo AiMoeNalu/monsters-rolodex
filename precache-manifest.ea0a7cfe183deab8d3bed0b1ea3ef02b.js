8dc1310555adbfbcfb99319366132bf3aeadf955
AiMoeNalu <48229683+AiMoeNalu@users.noreply.github.com>
2019-09-30 09:59:59 -0400

    Updates

---
+++ precache-manifest.ea0a7cfe183deab8d3bed0b1ea3ef02b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3220e59f4f42c23775002546f9bcf2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2ee713ab409fe3de9a9e",
+    "url": "/monsters-rolodex/static/css/main.5fafe453.chunk.css"
+  },
+  {
+    "revision": "6d4141b84795c5ee81ea",
+    "url": "/monsters-rolodex/static/js/2.1145dfaf.chunk.js"
+  },
+  {
+    "revision": "2ee713ab409fe3de9a9e",
+    "url": "/monsters-rolodex/static/js/main.b8a0a260.chunk.js"
+  },
+  {
+    "revision": "73e9046333f8d83eb620",
+    "url": "/monsters-rolodex/static/js/runtime-main.7b91b128.js"
+  }
+]);
